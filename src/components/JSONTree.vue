60d25e6bc1f56775cf2f208943ad6f034e6d93e2
childku <child-ku@qq.com>
2025-12-03 09:23:01 +0800

    init

---
+++ src/components/JSONTree.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="json-tree">
+    <JSONNode :data="data" :path="path" /> 
+  </div>
+</template>
+
+<script setup>
+import JSONNode from './JSONNode.vue'
+
+const props = defineProps({
+  data: {
+    type: [Object, Array, String, Number, Boolean, null],
+    default: null
+  },
+  path: {
+    type: String,
+    default: 'root'
+  }
+})
+</script>
+
+<style scoped>
+.json-tree {
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 13px;
+  line-height: 1.4;
+  color: #d4d4d4;
+  background-color: #1e1e1e;
+  padding: 16px;
+  border-radius: 4px;
+  overflow-x: auto;
+}
+</style>
